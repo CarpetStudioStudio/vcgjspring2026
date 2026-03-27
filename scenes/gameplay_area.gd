efb732abcce908a38fa168cc6c4ccf7f5e583b63
KaratePikachu <KaidenKeyZ@gmail.com>
2026-03-26 21:59:16 -0400

    F

--- scenes/gameplay_area.gd
+++ scenes/gameplay_area.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	
+	var mouse_position : Vector2 = $Board.get_mouse_position()
+	#$Dummy.position.x = 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
