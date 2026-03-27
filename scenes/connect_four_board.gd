26ac6ea53e3c65ef1a334d124828cc2c6cfea524
KaratePikachu <KaidenKeyZ@gmail.com>
2026-03-26 22:49:08 -0400

    Click

--- scenes/connect_four_board.gd
+++ scenes/connect_four_board.gd
@@ -6,11 +6,18 @@ var board : Board
 func _ready() -> void:
 	board = Board.new()
 
-func get_mouse_position() -> Vector2:
-	return $TileMap.get_local_mouse_position() 
+func drop_piece() -> void:
+	create_tween()
+	print("DROPPED")
+	
+func get_mouse_pos() -> Vector2:
+	return $TileMap.get_local_mouse_position()
 
 func grid_to_position(pos : Vector2i) -> Vector2:
-	return $TileMap.map_to_local(pos)
+	return $TileMap.map_to_local(pos)+$TileMap.global_position
 	
 func position_to_grid(pos : Vector2) -> Vector2i:
 	return $TileMap.local_to_map(pos)
+
+func snap(pos : Vector2) -> Vector2:
+	return grid_to_position(position_to_grid(pos))
--- scenes/gameplay_area.gd
+++ scenes/gameplay_area.gd
@@ -1,9 +1,30 @@
 extends Node2D
 
+@onready var topchip : Sprite2D = $Dummy
+var topchip_x : int = 3
+var num_trans : int = 0
+var queued_drop_x : int = -1
+
 func _process(delta: float) -> void:
-	var mouse_position : Vector2 = $Board.get_mouse_position()
-	#$Dummy.position.x = 
+	var new_x : int = $Board.position_to_grid($Board.get_mouse_pos()).x
+	new_x = clampi(new_x,0,$Board.board.COLS-1)
+	if topchip_x != new_x:
+		num_trans += 1
+		var tween : Tween = get_tree().create_tween()
+		tween.finished.connect(_trans_finished)
+		tween.set_trans(Tween.TRANS_QUAD)
+		var grid_pos : Vector2i = Vector2i(new_x,0)
+		tween.tween_property(topchip,"global_position:x",$Board.grid_to_position(grid_pos).x,0.25)
+		topchip_x = new_x
+
+func _trans_finished() -> void:
+	num_trans -= 1
+	if num_trans == 0 and queued_drop_x != -1:
+		$Board.drop_piece()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		pass
+	if event.is_action_pressed("drop_piece"):
+		if num_trans == 0:
+			$Board.drop_piece()
+		elif queued_drop_x == -1:
+			queued_drop_x = topchip_x
